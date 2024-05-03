f0d527d861d09cd308c886422025690618dcae72
Andre Batista Bezerra <andre3901@hotmail.com>
2024-05-03 10:45:11 -0300

    Criação de um método com um retorno.

--- meu-primeiro-projeto-java-dio/src/MinhaClasse.java
+++ meu-primeiro-projeto-java-dio/src/MinhaClasse.java
@@ -1,7 +1,13 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.println("Primeiro codigo Java no Bootcamp Santander.");
+        String primeiroNome = "Andre";
+        String segundoNome  = "Batista";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
     }
-    
+   
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do metodo: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
