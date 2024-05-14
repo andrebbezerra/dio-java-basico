b01b5e94e9de3ccd19341867324f2d41b87b3023
Andre Batista Bezerra <andre3901@hotmail.com>
2024-05-14 17:17:41 -0300

    Alteração para incluir os métodos

---
+++ meu-primeiro-projeto-java-dio/src/edu/andre/metodos/SmartTv.java
@@ -0,0 +1,13 @@
+package edu.andre.metodos;
+
+public class SmartTv {
+    boolean ligada  = false;
+    int canal       = 1;
+    int volume      = 25;
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        
+    }
+}
